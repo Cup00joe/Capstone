b8a116cef547c48514b1835f75fb42bdae5911f4
RonganBai <140551985+RonganBai@users.noreply.github.com>
2024-03-06 20:39:36 -0500

    Add files via upload

--- App/sidebar.js
+++ App/sidebar.js
@@ -7,6 +7,40 @@ import SettingIcon from '../picture/SettingIcon.png';
 import SignOut from '../picture/SignOut.png';
 import './sidebar.css';
 import { supabase } from '../supabase.js'; // Adjust the path here according to your actual setup
+import Settings from './settings.js';
+
+function Login({ 
+  handleLoginFormSubmit,
+  username,
+  password,
+  handleUsernameChange,
+  handlePasswordChange,
+  loginErrorMessage
+}) {
+  return (
+    <div className="login-popup">
+      {/* Login form */}
+      <form onSubmit={handleLoginFormSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        {loginErrorMessage && <div className="error-message">{loginErrorMessage}</div>}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
 
 function Sidebar({ setPage }) {
   // State management
@@ -20,14 +54,30 @@ function Sidebar({ setPage }) {
   const [password, setPassword] = useState(''); // Password state
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true'); // Login state
   const [loginErrorMessage, setLoginErrorMessage] = useState(''); // Login error message
+  const [isLoginSuccessVisible, setIsLoginSuccessVisible] = useState(false); // State to manage login success message visibility
 
+  const [showSettings, setShowSettings] = useState(false); // 控制设置组件的显示状态
   const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    // Check if user is logged in from local storage when component mounts
+    setIsLoggedIn(localStorage.getItem('isLoggedIn') === 'true');
+    // If user is not logged in, show login page
+    if (!isLoggedIn) {
+      setShowLoginPage(true);
+    }
+  }, []);
 
   useEffect(() => {
     // Update username and login status in local storage
     localStorage.setItem('username', username);
     localStorage.setItem('isLoggedIn', isLoggedIn);
   }, [username, isLoggedIn]);
+
+  useEffect(() => {
+    // Check if user is logged in from local storage when component mounts
+    setIsLoggedIn(localStorage.getItem('isLoggedIn') === 'true');
+  }, []);
 
   const handleMouseEnter = () => {
     setCollapsed(false);
@@ -41,7 +91,8 @@ function Sidebar({ setPage }) {
     setPage(pageName);
     setCurrentPage(pageName);
     setCollapsed(true);
-    // Hide the login page when clicking on other page buttons
+    setShowSettings(false); // 将设置组件隐藏
+    // 隐藏登录页面
     setShowLoginPage(false);
   };
 
@@ -61,35 +112,57 @@ function Sidebar({ setPage }) {
   };
 
   // Handle login form submission
+  // Handle login form submission
+  // Handle login form submission
   const handleLoginFormSubmit = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    try {
-      // Perform login authentication
-      let { data: login, error } = await supabase
-        .from('login')
-        .select('name, pin')
-        .eq('name', username) // Assuming username is the username
-        .single();
+  let login; // Define login variable here
+  let error; // Define error variable here
 
-      if (error) {
-        throw error;
-      }
+  try {
+    // Perform login authentication
+    ({ data: login, error } = await supabase
+      .from('login')
+      .select('name, pin')
+      .eq('name', username) // Assuming username is the username
+      .single());
 
-      // Check if a matching user record is found
-      if (!login || login.pin !== password) {
-        throw new Error('Incorrect username or password');
-      }
+    if (error) {
+      throw error;
+    }
 
-      // Login successful
-      setLoginErrorMessage('');
-      setIsLoggedIn(true); // Set login status to true
+    // Check if a matching user record is found
+    if (!login || login.pin !== password) {
+      throw new Error('Incorrect username or password');
+    }
 
-      // Close the login window
-      setShowLoginPage(false);
-    } catch (error) {
+    // Login successful
+    setLoginErrorMessage('');
+    setIsLoggedIn(true); // Set login status to true
+
+    // Set current page to Home page
+    setCurrentPage('Home');
+
+    // Close the login window
+    setShowLoginPage(false);
+
+    // Display success message
+    setIsLoginSuccessVisible(true);
+
+    // Hide the success message after 5 seconds
+    setTimeout(() => {
+      setIsLoginSuccessVisible(false);
+      window.location.reload();
+    }, 2000);
+    
+  } catch (error) {
+    if (!login) {
+      setLoginErrorMessage('Incorrect username or password');
+    } else {
       setLoginErrorMessage(error.message);
-      setIsLoggedIn(false); // Login failed, set login status to false
+    }
+    setIsLoggedIn(false); // Login failed, set login status to false
     }
   };
 
@@ -107,134 +180,112 @@ function Sidebar({ setPage }) {
   };
   
   return (
-    <div 
-      className={`sidebar ${collapsed ? 'collapsed' : 'expanded'}`}
-      ref={sidebarRef}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h3>HBA</h3>
-      <div className="sidebar-content">
-        {collapsed ? (
+    <div>
+      {!isLoggedIn && showLoginPage && (
+        <Login
+          handleLoginFormSubmit={handleLoginFormSubmit}
+          username={username}
+          password={password}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+          loginErrorMessage={loginErrorMessage}
+        />
+      )}
+  
+      {isLoggedIn && (
+        <div 
+          className={`sidebar ${collapsed ? 'collapsed' : 'expanded'}`}
+          ref={sidebarRef}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <>
-          {!isLoggedIn && (
-          // Show the "Sign In" button if not logged in
-          <button onClick={handleShowLoginPage} className="menu-button">Sign In HBA</button>
-          )}
-          {isLoggedIn && (
-          // Show page buttons and logout button if logged in
-          <>
-            <button
-              className={`menu-button ${currentPage === 'Home' ? 'active' : ''}`}
-            >
-              <img src={homeIcon} alt="Home" />
-            </button>
-            <button
-              className={`menu-button ${currentPage === 'Customer Information Input' ? 'active' : ''}`}
-            >
-              <img src={customerInputIcon} alt="Customer Input" />
-            </button>
-            <button
-              className={`menu-button ${currentPage === 'Customer Information' ? 'active' : ''}`}
-            >
-              <img src={customerInfoIcon} alt="Customer Info" />
-            </button>
-            <button
-              className={`menu-button ${currentPage === 'Appointment' ? 'active' : ''}`}
-            >
-              <img src={appointmentIcon} alt="Appointment" />
-            </button>
-            <button
-              className={`menu-button ${currentPage === 'Settings' ? 'active' : ''}`}
-            >
-              <img src={SettingIcon} alt="Settings" />
-            </button>
-            <button
-            className={`menu-button ${currentPage === 'SignOut' ? 'active' : ''}`}
-            >
-              <img src={SignOut} alt="SignOut" />
-            </button>
+            <h3>HBA</h3>
+            <div className="sidebar-content">
+              {collapsed ? (
+                <>
+                  <button
+                    className={`menu-button ${currentPage === 'Home' ? 'active' : ''}`}
+                  >
+                    <img src={homeIcon} alt="Home" />
+                  </button>
+                  <button
+                    className={`menu-button ${currentPage === 'Customer Information Input' ? 'active' : ''}`}
+                  >
+                    <img src={customerInputIcon} alt="Customer Input" />
+                  </button>
+                  <button
+                    className={`menu-button ${currentPage === 'Customer Information' ? 'active' : ''}`}
+                  >
+                    <img src={customerInfoIcon} alt="Customer Info" />
+                  </button>
+                  <button
+                    className={`menu-button ${currentPage === 'Appointment' ? 'active' : ''}`}
+                  >
+                    <img src={appointmentIcon} alt="Appointment" />
+                  </button>
+                  <button
+                    className={`menu-button ${currentPage === 'Settings' ? 'active' : ''}`}
+                  >
+                    <img src={SettingIcon} alt="Settings" />
+                  </button>
+                  <button
+                    className={`menu-button ${currentPage === 'SignOut' ? 'active' : ''}`}
+                    onClick={handleLogout}
+                  >
+                    <img src={SignOut} alt="SignOut" />
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setPageAndCloseMenu('Home')} className={`expanded-menu-button ${currentPage === 'Home' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={homeIcon} alt="Home" />
+                      <span>Home</span>
+                    </div>
+                  </button>
+                  <button onClick={() => setPageAndCloseMenu('Customer Information Input')} className={`expanded-menu-button ${currentPage === 'Customer Information Input' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={customerInputIcon} alt="Customer Input" />
+                      <span>Customer Input</span>
+                    </div>
+                  </button>
+                  <button onClick={() => setPageAndCloseMenu('Customer Information')} className={`expanded-menu-button ${currentPage === 'Customer Information' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={customerInfoIcon} alt="Customer Info" />
+                      <span>Customer Info</span>
+                    </div>
+                  </button>
+                  <button onClick={() => setPageAndCloseMenu('Appointment')} className={`expanded-menu-button ${currentPage === 'Appointment' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={appointmentIcon} alt="Appointment" />
+                      <span>Appointment</span>
+                    </div>
+                  </button>
+                  <button onClick={() => setPageAndCloseMenu('Settings')} className={`expanded-menu-button ${currentPage === 'Settings' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={SettingIcon} alt="Settings" />
+                      <span>Setting</span>
+                    </div>
+                  </button>
+                  <button onClick={handleLogout} className={`expanded-menu-button ${currentPage === 'SignOut' ? 'active' : ''}`}>
+                    <div className="button-content">
+                      <img src={SignOut} alt="SignOut" />
+                      <span>Sign Out</span>
+                    </div>
+                  </button>
+                </>
+              )}
+            </div>
+            {/* Display success or error message for login */}
+            {isLoginSuccessVisible && <div className="login-success-message">Login successful!</div>}
+            {showSettings && <Settings username={username} />}
           </>
-          )}
-      </>
-      ):(
-      <>
-      {!isLoggedIn && (
-          // Show the "Sign In" button if not logged in
-          <button onClick={handleShowLoginPage} className="menu-button">Sign In HBA</button>
-          )}
-          {isLoggedIn && (
-            <>
-            <button onClick={() => setPageAndCloseMenu('Home')} className={`expanded-menu-button ${currentPage === 'Home' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={homeIcon} alt="Home" />
-                <span>Home</span>
-              </div>
-            </button>
-            <button onClick={() => setPageAndCloseMenu('Customer Information Input')} className={`expanded-menu-button ${currentPage === 'Customer Information Input' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={customerInputIcon} alt="Customer Input" />
-                <span>Customer Input</span>
-              </div>
-            </button>
-            <button onClick={() => setPageAndCloseMenu('Customer Information')} className={`expanded-menu-button ${currentPage === 'Customer Information' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={customerInfoIcon} alt="Customer Info" />
-                <span>Customer Info</span>
-              </div>
-            </button>
-            <button onClick={() => setPageAndCloseMenu('Appointment')} className={`expanded-menu-button ${currentPage === 'Appointment' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={appointmentIcon} alt="Appointment" />
-                <span>Appointment</span>
-              </div>
-            </button>
-            <button onClick={() => setPageAndCloseMenu('Settings')} className={`expanded-menu-button ${currentPage === 'Settings' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={SettingIcon} alt="Settings" />
-                <span>Setting</span>
-              </div>
-            </button>
-            <button onClick={handleLogout} className={`expanded-menu-button ${currentPage === 'SignOut' ? 'active' : ''}`}>
-              <div className="button-content">
-                <img src={SignOut} alt="SignOut" />
-                <span>Sign Out</span>
-              </div>
-            </button>
-              </>
-            )}
-          </>
-        )}
-      </div>
-      {/* Display success or error message for login */}
-      {isLoggedIn && <div className="login-message success">Login successful!</div>}
-      {loginErrorMessage && <div className="login-message error">{loginErrorMessage}</div>}
-      {/* Conditional rendering of login page */}
-      {showLoginPage && (
-        <div className="login-popup">
-          {/* Login form */}
-          <form onSubmit={handleLoginFormSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            {loginErrorMessage && <div className="error-message">{loginErrorMessage}</div>}
-            <button type="submit">Login</button>
-          </form>
         </div>
       )}
     </div>
   );
+  
 }
 
 export default Sidebar;
