ef5e4db81e6578b96e00f6f8763526e9458bca7e
RonganBai <140551985+RonganBai@users.noreply.github.com>
2024-03-10 18:39:43 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './PageFunction/sidebar';
 import RenderCalendar from './PageFunction/HomeCalendar';
-import Settings from './PageFunction/register';
+import Register from './PageFunction/register';
+import SunriseSunsetComponent from './PageFunction/SunriseSunsetComponent';
 import moment from 'moment-timezone';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import AddressInput from './PageFunction/Address'; // 导入 AddressInput 组件
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to manage user login status
-  const [isGoogleLoggedIn, setIsGoogleLoggedIn] = useState(false); // State to manage Google login status
-  const [page, setPage] = useState('Home'); // State to manage current page
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isGoogleLoggedIn, setIsGoogleLoggedIn] = useState(false);
+  const [page, setPage] = useState('Home');
+  const [coordinates, setCoordinates] = useState('');
+  const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD')); // 默认为当天日期
 
-  const session = useSession(); // Get session information using auth-helpers-react
-  const supabase = useSupabaseClient(); // Get Supabase client object
-  const [appUsername, setAppUsername] = useState('');
+  const session = useSession();
+  const supabase = useSupabaseClient();
 
   useEffect(() => {
     const storedIsLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
@@ -23,55 +26,56 @@ function App() {
     setIsGoogleLoggedIn(storedGoogleLoggedIn);
 
     if (session?.provider_token) {
-        setIsGoogleLoggedIn(true);
-        console.log('Google is logged in');
+      setIsGoogleLoggedIn(true);
+      console.log('Google is logged in');
     } else {
-        console.log('Google is not logged in');
+      console.log('Google is not logged in');
     }
-}, [session]);
+  }, [session]);
 
-useEffect(() => {
-  console.log('Login status changed(App):', isLoggedIn);
-}, [isLoggedIn]);
+  useEffect(() => {
+    console.log('Login status changed(App):', isLoggedIn);
+  }, [isLoggedIn]);
 
   async function googleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-            scopes: 'https://www.googleapis.com/auth/calendar'
-        }
+      provider: 'google',
+      options: {
+        scopes: 'https://www.googleapis.com/auth/calendar'
+      }
     });
     if (error) {
-        alert("Error logging in to Google provider with Supabase");
-        console.log(error);
+      alert("Error logging in to Google provider with Supabase");
+      console.log(error);
     } else {
-        // Set Google login status to true after successful login
-        setIsGoogleLoggedIn(true);
-        // Save Google login status to local storage
-        localStorage.setItem('isGoogleLoggedIn', 'true');
+      setIsGoogleLoggedIn(true);
+      localStorage.setItem('isGoogleLoggedIn', 'true');
     }
-};
+  };
 
-async function googlesignOut() {
+  async function googlesignOut() {
     await supabase.auth.signOut();
-    // Reset isLoggedIn and isGoogleLoggedIn states after successful logout
     setIsLoggedIn(false);
     setIsGoogleLoggedIn(false);
-    // Clear Google login status from local storage
     localStorage.removeItem('isGoogleLoggedIn');
     window.location.reload();
-}
+  }
 
-const handleUsernameChange = (username) => {
-  setAppUsername(username);
-};
+  const handleDateSelect = (date) => {
+    console.log('Selected date:', date.format('YYYY-MM-DD'));
+    setSelectedDate(date.format('YYYY-MM-DD'));
+  };
+
+  // 处理地址提交
+  const handleAddressSubmit = (newCoords) => {
+    console.log('坐标:', newCoords); // 修正此处的变量名为 newCoords
+    setCoordinates(newCoords);
+  };
 
   return (
     <div className="App">
-      {/* Pass isLoggedIn state and handling methods to Sidebar component */}
       <Sidebar isLoggedIn={isLoggedIn} setPage={setPage}/>
-      
-      {/* Render content based on page state */}
+
       <div className="main-content" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         {isLoggedIn ? (
           <>
@@ -80,33 +84,47 @@ const handleUsernameChange = (username) => {
                 {page === 'Settings' && (
                   <div className="settings-container">
                     <h2>Settings</h2>
-                    <Settings />
-                    <button onClick={googlesignOut} className="logout-button" style={{ position: 'absolute', bottom: '20px', right: '20px' }}>Sign Out</button>
+                    <Register username/>
                   </div>
                 )}
                 {page === 'Customer Information Input' && (
                   <div className="home-container">
                     <h2>Welcome, !</h2>
                     <p>{`Today is ${moment().format('YYYY-MM-DD')}`}</p>
-                    {/* Add user input component here */}
                   </div>
                 )}
                 {page === 'Home' && (
-                  <div className="home-container">
-                    <h2>Welcome!</h2>
-                    <p>{`Today is ${moment().format('YYYY-MM-DD')}`}</p>
-                    {/* Add calendar component here */}
-                    <RenderCalendar />
+                  <div className="calendar-parent-container">
+                    <div className="calendar-container">
+                      <RenderCalendar onDateSelect={handleDateSelect} />
+                    </div>
+                    <div className="address-wooster-container">
+                      <div className="address-container">
+                        <AddressInput onAddressSubmit={handleAddressSubmit} />
+                      </div>
+                      <div className="wooster-time-container pointer-events-none">
+                        <SunriseSunsetComponent
+                          coordinates={coordinates}
+                          selectedDate={selectedDate}
+                        />
+                      </div>
+                    </div>
                   </div>
                 )}
               </>
             ) : (
-              // Content when not logged in, can be adjusted as needed
               <div className="login-container">
                 <h2>Please Login</h2>
-                <button onClick={googleSignIn} className="logon-button">Sign In</button>
+                <button onClick={googleSignIn} className="logon-button">Google Sign In</button>
               </div>
             )}
+            <button 
+              onClick={isGoogleLoggedIn ? googlesignOut : googleSignIn} 
+              className="google-signin-button"
+              style={{ backgroundColor: isGoogleLoggedIn ? 'green' : 'red' }}
+            >
+              {isGoogleLoggedIn ? 'Google Sign Out' : 'Google Sign In'}
+            </button>
           </>
         ) : (
           <div>
